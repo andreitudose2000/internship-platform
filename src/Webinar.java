0aa12398e20a819576aa9d1dad491e48808e3571
Andrei Tudose <andreitudose2000@gmail.com>
2021-04-04 12:37:29 +0300

    Creat clasa Webinar

---
+++ src/Webinar.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Webinar {
+
+    private String name;
+    private LocalDate startDate;
+    private LocalDate finishDate;
+    private LocalTime startTime;
+    private LocalTime finishTime;
+
+}
